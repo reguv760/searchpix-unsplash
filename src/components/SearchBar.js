dfc967c660a6299a7cf0429712a18665679172e4
reguv760 <reguv760@gmail.com>
2019-09-17 18:10:22 -0700

    add inputClick functionality + submit button

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,14 +4,33 @@ import React from 'react';
 //this means data is stored within the component
 //not on the DOM level
 
+const defaultInputText = "Search for any image..."
+
 class SearchBar extends React.Component
 {
-    state = { term: 'Search for any image...' }
+    constructor(props)
+    {
+        super(props);
+        this.searchBarInput = React.createRef();
+    }
+
+
+    state = { term: defaultInputText }
 
     onFormSubmit = (e) =>
     {
         e.preventDefault();
         this.props.onSubmit(this.state.term);
+    }
+
+    onInputClick = (e) =>
+    {
+        e.preventDefault();
+        
+        if (this.state.term === defaultInputText)
+        {               
+            this.setState({ term: '' });            
+        }
     }
 
     render()
@@ -20,10 +39,14 @@ class SearchBar extends React.Component
             <div className="ui segment">
                 <form className="ui form" onSubmit={this.onFormSubmit} >
                     <div className="field">
+
                         <label>Image Search</label>
                         <input type="text" value={this.state.term } onChange={
                         (e) => this.setState({term:e.target.value})
-                        }/>
+                        } onClick={this.onInputClick}/>
+                
+                        <button type="submit" style={{marginTop:"10px"}}>Submit</button>
+                    
                     </div>
                 </form>
             </div>
